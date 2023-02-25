10344160ffe401063a7100d17956c686bde6c61b
Amadeus Alerta <efe3365@gmail.com>
2023-02-25 10:20:18 +0300

    Basics
    Just basics

---
+++ python_temelleri/type-conversion.py
@@ -0,0 +1,36 @@
+"""
+x =input('1.Sayı:')
+y =input('2.Sayı:')
+print(type(x))
+print(type(y))
+toplam=int(x)+int(y)
+print(toplam)
+"""
+
+# x = 5             
+# y =2.5
+# name= 'Oguz'
+# isOnline=True
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# Type Conversion
+
+#int to float
+
+# x=float(x)
+# print(x)
+# print(type(x))
+
+pi_sayisi= 3.14
+yari_cap=input("Yarı Çap:")
+yari_cap=float(yari_cap)
+alan =pi_sayisi*(yari_cap**2)
+print(alan)
+cevre=2*pi_sayisi*yari_cap
+print(cevre)
+
+
