8f4b12cefdb0e5cdd7fedca4c495ccce81782ddc
Amadeus Alerta <efe3365@gmail.com>
2023-02-26 17:01:12 +0300

    Little Update and Learnings
    Little Update and Learnings

---
+++ python_temelleri/strings_demo.py
@@ -0,0 +1,11 @@
+example01="This is a sss simple sentence."
+example02="Is this a sentence ?"
+result=len(example02)
+print(result)
+result_01=example01[10:13]
+print(result_01)
+result_02=example02[::-1]
+print(result_02)
+simple="Hello world"
+simple=simple[0:6]+'W'+simple[-4:]
+print(simple)
