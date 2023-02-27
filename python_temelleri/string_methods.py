5ca57b392661ec54e629f9f215373ac0b795928c
Amadeus Alerta <efe3365@gmail.com>
2023-02-27 14:02:47 +0300

    String Methods
    String Methods

---
+++ python_temelleri/string_methods.py
@@ -0,0 +1,8 @@
+message='Rome Total War is a good strategy game.'
+message=message.upper()
+# message=message.lower()
+# message=message.capitalize()
+# message=message.title()
+# message=message.strip()
+# message=message.split()
+print(message)
