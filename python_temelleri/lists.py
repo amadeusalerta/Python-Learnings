025bd7ca78f742f2135c75a77c34debfbaa3bacb
Amadeus Alerta <efe3365@gmail.com>
2023-03-02 10:36:05 +0300

    Update to lists 1.1

--- python_temelleri/lists.py
+++ python_temelleri/lists.py
@@ -1,2 +1,11 @@
-message='Hello There.My Name is Amadeus Alerta'.split()
-print(message)
+# message='Hello There.My Name is Amadeus Alerta'.split()
+# print(message[0])
+
+# my_list=[1,2,3,4,5,6]
+# my_list=['bir',2,True,5.6]
+# print(my_list)
+
+list_01=['one','two','three']
+list_02=['four','five','six']
+string_numbers_list=list_01+list_02
+print(string_numbers_list)
