8816b51c89d37063c8ff0ed9fe334bf554356a66
Amadeus Alerta <efe3365@gmail.com>
2023-03-02 10:58:58 +0300

    Update 1.1

--- python_temelleri/lists.py
+++ python_temelleri/lists.py
@@ -5,7 +5,21 @@
 # my_list=['bir',2,True,5.6]
 # print(my_list)
 
-list_01=['one','two','three']
-list_02=['four','five','six']
-string_numbers_list=list_01+list_02
-print(string_numbers_list)
+# list_01=['one','two','three']
+# list_02=['four','five','six']
+# string_numbers_list=list_01+list_02
+# print(string_numbers_list)
+
+cars=['BMW','Mercedes Benz','Opel','Wolkswagen','Porshe']
+# print(len(cars))
+# print(cars[0])
+# print(cars[4])
+# cars[-1]='Toyota'
+# print(cars)
+result='Mercedes' in cars
+result=cars[-2]
+result=cars[0:3]
+result=cars+['Audi','Togg']
+del cars[-1]
+result=cars
+print(result)
