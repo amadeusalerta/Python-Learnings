236c51b72b61cea56a2937785844f6990c5772fc
Amadeus Alerta <efe3365@gmail.com>
2023-03-02 12:07:32 +0300

    List Methods

---
+++ python_temelleri/list_methods.py
@@ -0,0 +1,15 @@
+numbers=[1,23,4,5,65,413,91]
+letters=['a','j','h','l','u','s','x']
+# val=min(numbers)
+# val=max(numbers)
+# val_01=min(letters)
+# val_01=max(letters)
+# print(val)
+# print(val_01)
+# numbers.append(7) #Add numvers to the end of the list
+# numbers.insert(3,34) #Add numbers to the selected index 
+# numbers.pop(1) #Remove numbers that you assigned an index number
+# numbers.remove(23) #Search number that we choose and deleting the number
+# numbers.sort #Sorts the list
+numbers.reverse #Reverses the list
+print(numbers)
