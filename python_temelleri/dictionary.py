670b4b8737e379c37b3198eb5d08060fb733e97c
Amadeus Alerta <efe3365@gmail.com>
2023-03-24 11:01:30 +0300

    Dictionary Lecture

---
+++ python_temelleri/dictionary.py
@@ -0,0 +1,30 @@
+
+# sehirler=['Adana','Mersin','Ankara']
+# plakalar=[1,33,6]
+
+# print (plakalar[sehirler.index('Adana')])
+# print (plakalar[sehirler.index('Mersin')])
+
+# plakalar={'Adana':'1','Mersin':'33'}
+# print(plakalar['Mersin'])
+# plakalar['Ankara']=6
+# plakalar['Adana']='Amogus'
+
+users={
+    'amadeusalerta':{
+    'roles':'admin',
+    'age':21,
+    'living':'yes',
+    'favouritecar':'audi_tt',
+    'phone_num':+905789456132,
+    },
+    'abrahamsweetvoice':{
+    'roles':'user',
+    'age':57,
+    'living':'yes',
+    'favouritecar':'toyota_supra',
+    'phone_num':+905396786741,
+    },
+}
+
+print(users['amadeusalerta']['roles'])
