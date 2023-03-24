e8d63c9ba75a2dc56812db8f38ba406024c268b2
Amadeus Alerta <efe3365@gmail.com>
2023-03-24 10:33:40 +0300

    Tuple Lecture
    Tuple Lecture

---
+++ python_temelleri/tuple.py
@@ -0,0 +1,11 @@
+
+list=[1,2,3]
+tuple=(1,'Abraham',3)
+
+# print(type(list))
+# print(type(tuple))
+# print(len(list))
+# print(len(tuple))
+
+list=['Fikret','Sercan'] #Önceki içeriği silip yenisini ekler.
+tuple=('Damla','Ayşe')   #Önceki içeriği silip yenisini ekler.
