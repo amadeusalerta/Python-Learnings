fbb15c321fff92750ae84f5f156b78b609703670
Amadeus Alerta <efe3365@gmail.com>
2023-03-28 10:28:12 +0300

    For Loop

---
+++ python_temelleri/for_loop.py
@@ -0,0 +1,19 @@
+
+# numbers=[1,2,3,4,5]
+
+# for num in numbers:
+#     print(num)
+
+# names=['Omar','Umut','Sedef']
+
+# for name in names:
+#     print(f'My Name is {name}.' )
+
+# name='Abraham Sweetvoice'
+
+# for n in name:
+#     print(n)
+
+d={'k1':1,'k2':2,'k3':3}
+for key,value in d.items():
+    print(key,value)
