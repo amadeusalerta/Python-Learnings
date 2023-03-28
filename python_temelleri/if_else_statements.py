63bcd08cde1f73ff3913b587092ad8e16b785791
Amadeus Alerta <efe3365@gmail.com>
2023-03-28 10:13:36 +0300

    If-Else-Elif Statements

---
+++ python_temelleri/if_else_statements.py
@@ -0,0 +1,13 @@
+
+isLoggedUserID='AbrahamSweet'
+isLoggedPassword='112233'
+
+Logged_User=(isLoggedUserID=='AbrahamSweet')and(isLoggedPassword=='111233')
+
+if Logged_User:
+    print('Welcome aboard agent!')
+    
+else:
+    print('Kanye West in Paris')
+
+# elif ise direkt ya biliyom zaten 3.seçenek olursa falan koyuyon
