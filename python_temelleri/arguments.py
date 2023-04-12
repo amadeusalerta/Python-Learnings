bde759156a06c4f5487d1a776bb3d7a7dca82259
Amadeus Alerta <efe3365@gmail.com>
2023-04-12 07:02:18 +0300

    Arguments

---
+++ python_temelleri/arguments.py
@@ -0,0 +1,16 @@
+
+# def changeName(n):
+#     n='Alexa'
+
+# name='John'
+
+# changeName(name)
+# print(name)
+
+def change(n):
+    n[0]='Amsterdam'
+cities=['Berlin','Roma']
+
+change(cities)
+
+print(cities)
