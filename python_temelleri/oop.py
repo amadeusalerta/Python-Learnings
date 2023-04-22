cb90f0efcc4e93bce7911f46d218952fa7e1c20b
Amadeus Alerta <efe3365@gmail.com>
2023-04-22 18:51:03 +0300

    OOP
    Object Oriented Programming basics

---
+++ python_temelleri/oop.py
@@ -0,0 +1,15 @@
+#Object Oriented Programming - Nesne Tabanlı Programlar
+
+#class
+
+list1=[1,2,3]
+list2=[5,7,9]
+
+result01=type(list1)
+result02=type(list2)
+
+list1.clear(1)
+
+print (result01)
+
+#instance
