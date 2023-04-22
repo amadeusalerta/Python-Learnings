ccefb03368ad987919e0bad37859142563c933e0
Amadeus Alerta <efe3365@gmail.com>
2023-04-22 19:22:32 +0300

    Class Basics
    Class basics about class methods.

--- python_temelleri/class01.py
+++ python_temelleri/class01.py
@@ -1,1 +1,34 @@
-ÿþ
+
+#Class
+
+class Person:
+ pass
+    #Class Attirbutes
+
+ address='No Information'
+    #Constructor(YapÄ±cÄ± Method)
+ def __init__(elf,name,year):
+  
+    #Object Attirbutes
+    elf.name=name
+    elf.year=year
+    print('Init Method is working well')
+    #Methods
+
+#Object(Instance)
+
+p1=Person('David ',2001)
+p2=Person('Star ',1999)
+
+#Updating Objects
+
+p1.name='Mason '
+p1.address='Ohio'
+
+#Accesing Objects
+ 
+print(f'name:{p1.name}year:{p1.year}address:{p1.address}')
+print(f'name:{p2.name}year:{p2.year}address:{p2.address}')
+
+print(type(p1))
+print(type(p2))
